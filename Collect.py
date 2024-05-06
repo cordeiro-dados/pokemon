a769be5d3cc8c1e9c79138b4bda3d45e99fd4b76
cordeiro-dados <cordeiro-dados>
2024-05-06 16:45:53 +0000

    Coleta de Dados da API do Pokemon

---
+++ Collect.py
@@ -0,0 +1,63 @@
+# Databricks notebook source
+import requests
+import datetime
+import json
+
+class Collector:
+
+    def __init__(self, url):
+        self.url = url
+        self.instance = url.strip("/").split("/")[-1]
+
+    def get_endpoint(self,**kwargs):
+        resp = requests.get(self.url, params= kwargs)
+        return resp
+    
+    def save_data(self,data):
+        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S.%f")
+        data['ingestion_date'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        filename = f"/dbfs/datalake/pokemon/{self.instance}/{now}.json"
+        with open(filename, "w") as open_file:
+            json.dump(data, open_file)
+
+    def get_and_save(self, **kwargs):
+        resp = self.get_endpoint(**kwargs)
+        if resp.status_code == 200:
+            data = resp.json()
+            self.save_data(data)
+            return data
+        else:
+            return {}
+        
+    def auto_exec(self, limit=100):
+        offset = 0
+        while True:
+            data = self.get_and_save(limit=limit, offset=offset)
+
+            if data['next'] == None:
+                break
+
+            offset+=limit
+
+# COMMAND ----------
+
+url = "https://pokeapi.co/api/v2/pokemon/"
+
+collector = Collector(url)
+
+collector.auto_exec()
+
+# COMMAND ----------
+
+df = spark.read.json("/datalake/pokemon/pokemon")
+df.createOrReplaceTempView("pokemon")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select
+# MAGIC   ingestion_date,
+# MAGIC   poke.*
+# MAGIC from
+# MAGIC   pokemon
+# MAGIC lateral view explode(results) as poke --lateral view, com explode, ele quebra a result, trazendo toda informacao do dicionario da variavel results
